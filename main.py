ef19094cf7defdee7de80cb64fbf10f83d9961eb
Félix Ernesto Charry Pastrana <feecharrypa@unal.edu.co>
2021-06-17 01:50:58 -0500

    fix some pep-8 problems

--- main.py
+++ main.py
@@ -87,10 +87,6 @@ data[((data['category'].isna()) | (data['category'] == 'Creators & Celebrities')
      & (data['betweenness_centrality'] != 0)
      & (data['centrality'] > 0.28)].sort_values(by=['betweenness_centrality'], ascending=False)
 
-new_red_users = list(data[((data['category'].isna()) | (data['category'] == 'Creators & Celebrities'))
-     & (data['betweenness_centrality'] != 0)
-     & (data['centrality'] > 0.28)]['account'])
-
 # New network --------
 new_red_users = list(data[((data['category'].isna()) | (data['category'] == 'Creators & Celebrities'))
                           & (data['betweenness_centrality'] != 0)
@@ -114,11 +110,11 @@ for seed in ['seed_1', 'seed_2', 'seed_3', 'seed_4']:
                 DG.nodes[j][i] = seed_dic[seed][j][i]
 
 # --- Plot 1
-pos = nx.spring_layout(DG) # https://www.datacamp.com/community/tutorials/social-network-analysis-python
+pos = nx.spring_layout(DG)  # https://www.datacamp.com/community/tutorials/social-network-analysis-python
 betCent = nx.betweenness_centrality(DG, normalized=True, endpoints=True)
-node_color = [20000.0 * DG.degree(v) for v in DG]
+node_color = [20000.0*DG.degree(v) for v in DG]
 node_size = [v * 10000 for v in betCent.values()]
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(20, 20))
 nx.draw_networkx(DG, pos=pos, with_labels=True,
                  node_color=node_color,
                  node_size=node_size)
@@ -126,11 +122,11 @@ plt.savefig('testplot.png')
 
 # ---- Plot 2
 # Draw graph with varying node color, node size, and edge width
-plt.figure(figsize=(20,10))
+plt.figure(figsize=(20, 10))
 
 node_color = [DG.degree(v) for v in DG]
 node_size = [0.0005*nx.get_node_attributes(DG, 'nofollowers')[v] for v in DG]
-#edge_width = [0.0015*DG[u][v]['nofollowees'] for u,v in DG.edges()]
+# edge_width = [0.0015*DG[u][v]['nofollowees'] for u,v in DG.edges()]
 
 pos = nx.circular_layout(DG)
 nx.draw_networkx(DG, pos, node_size=node_size,
@@ -139,9 +135,6 @@ nx.draw_networkx(DG, pos, node_size=node_size,
 plt.axis('off')
 plt.tight_layout()
 plt.savefig('testplot_2.png')
-
-
-
 
 print(data)
 print(len(new_red_users))
