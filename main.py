8a315b1bfa9be5660fea96485c6da861af1b6cda
Félix Ernesto Charry Pastrana <feecharrypa@unal.edu.co>
2021-06-17 01:48:29 -0500

    create new network and plot it

--- main.py
+++ main.py
@@ -89,7 +89,60 @@ data[((data['category'].isna()) | (data['category'] == 'Creators & Celebrities')
 
 new_red_users = list(data[((data['category'].isna()) | (data['category'] == 'Creators & Celebrities'))
      & (data['betweenness_centrality'] != 0)
-     & (data['centrality'] > 0.28)])
+     & (data['centrality'] > 0.28)]['account'])
+
+# New network --------
+new_red_users = list(data[((data['category'].isna()) | (data['category'] == 'Creators & Celebrities'))
+                          & (data['betweenness_centrality'] != 0)
+                          & (data['centrality'] > 0.28)]['account'])
+red_total_count = len(new_red_users)
+
+
+DG = nx.DiGraph()
+
+i = 'nofollowees'
+for seed in ['seed_1', 'seed_2', 'seed_3', 'seed_4']:
+    for user_red in new_red_users:
+        for user_seed_2 in list(seed_dic[seed].keys()):
+            if (user_red in seed_dic[seed][user_seed_2]['followees']) & (user_seed_2 in new_red_users):
+                DG.add_edges_from([(user_seed_2, user_red)])
+
+for seed in ['seed_1', 'seed_2', 'seed_3', 'seed_4']:
+    for i in ['bussines', 'category', 'nofollowees', 'nofollowers', 'noposts', 'private', 'verified']:
+        for j in list(seed_dic[seed].keys()):
+            if j in new_red_users:
+                DG.nodes[j][i] = seed_dic[seed][j][i]
+
+# --- Plot 1
+pos = nx.spring_layout(DG) # https://www.datacamp.com/community/tutorials/social-network-analysis-python
+betCent = nx.betweenness_centrality(DG, normalized=True, endpoints=True)
+node_color = [20000.0 * DG.degree(v) for v in DG]
+node_size = [v * 10000 for v in betCent.values()]
+plt.figure(figsize=(20,20))
+nx.draw_networkx(DG, pos=pos, with_labels=True,
+                 node_color=node_color,
+                 node_size=node_size)
+plt.savefig('testplot.png')
+
+# ---- Plot 2
+# Draw graph with varying node color, node size, and edge width
+plt.figure(figsize=(20,10))
+
+node_color = [DG.degree(v) for v in DG]
+node_size = [0.0005*nx.get_node_attributes(DG, 'nofollowers')[v] for v in DG]
+#edge_width = [0.0015*DG[u][v]['nofollowees'] for u,v in DG.edges()]
+
+pos = nx.circular_layout(DG)
+nx.draw_networkx(DG, pos, node_size=node_size,
+                 node_color=node_color, alpha=0.7, with_labels=True, edge_color='.4', cmap=plt.cm.Blues)
+
+plt.axis('off')
+plt.tight_layout()
+plt.savefig('testplot_2.png')
+
+
+
+
 print(data)
 print(len(new_red_users))
 '''
